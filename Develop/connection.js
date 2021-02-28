a2958c072e3cadf8d58ac18fa9f0bc1d7ec90fff
Ben T <ben.trevethan@gmail.com>
2021-02-28 13:42:36 +1030

    added package 'uniqid' for assigning unique ids, added working post method

--- Develop/connection.js
+++ Develop/connection.js
@@ -14,5 +14,12 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, `/public/index.html
 
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, `/db/db.json`)));
 
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, `/public/index.html`)));
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    console.log(newNote);
+    res.json({'msg': 'working'})
+})
 
 app.listen(PORT, () => console.log(`listening to port ${PORT}`));
