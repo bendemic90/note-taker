aa0fde359161b00ffab8c5e346804cbe6644663c
Ben T <ben.trevethan@gmail.com>
2021-02-28 21:53:33 +1030

    updated server file and testing package.json updates

--- Develop/connection.js
+++ Develop/connection.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+let note = [];
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
@@ -19,7 +22,13 @@ app.get('*', (req, res) => res.sendFile(path.join(__dirname, `/public/index.html
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     console.log(newNote);
-    res.json({'msg': 'working'})
-})
+    // fs.appendFile(`./db/db.json`, newNote, (err) => {
+    //     if (err) throw err;
+    //     console.log(`data appended`);
+    // });
+    res.json(newNote);
+});
 
 app.listen(PORT, () => console.log(`listening to port ${PORT}`));
+
+//module.exports = main;
