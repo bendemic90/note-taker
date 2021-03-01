b8d7bf5c23a7aaafac79e6cda82f161c71592412
Ben T <ben.trevethan@gmail.com>
2021-03-01 16:07:44 +1030

    saving note array on restart working now

--- Develop/connection.js
+++ Develop/connection.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const uniqid = require('uniqid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let note = [];
+const note = [];
+
+//------------------------
+//on initialisation, pushes db.json to note[]
+const y = () => {
+fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+    if (err) console.error;
+    let x = JSON.parse(data);
+    for (i = 0; i < x.length; i++) {
+            note.push(x[i]);
+        }
+    })
+}
+
+y()
+//-------------------------
+
+
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -21,14 +39,12 @@ app.get('*', (req, res) => res.sendFile(path.join(__dirname, `/public/index.html
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    console.log(newNote);
-    // fs.appendFile(`./db/db.json`, newNote, (err) => {
-    //     if (err) throw err;
-    //     console.log(`data appended`);
-    // });
+    note.push(newNote);
     res.json(newNote);
+    fs.writeFile(`./db/db.json`, JSON.stringify(note), (err) => {
+        if (err) throw err;
+        console.log(`done`)
+    })
 });
 
 app.listen(PORT, () => console.log(`listening to port ${PORT}`));
-
-//module.exports = main;
